a9ed6d3c4395ae5ea14fed5580ce432bf14da3e9
pomo <bosl95@naver.com>
2021-07-05 23:33:46 +0900

    refactor: 불필요한 import문 제거

--- src/test/java/qna/BaseTimeEntityTest.java
+++ src/test/java/qna/BaseTimeEntityTest.java
@@ -1,11 +1,9 @@
 package qna;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import qna.answer.Answer;
 import qna.answer.AnswerRepository;
--- src/test/java/qna/user/UserTest.java
+++ src/test/java/qna/user/UserTest.java
@@ -1,7 +1,6 @@
 package qna.user;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
